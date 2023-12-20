fa39bd72e34625b23eae1be29b40ce9f7bfc82be
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-12-20 10:10:03 -0700

    Creating Express Middleware

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const { PORT } = require('./config');
+const expressMiddleware = require('./middleware/express.js');
+const passportMiddleware = require('./middleware/passport.js');
 
 async function startServer() {
 
-    // app.get('/', (req, res) => {
-    //     res.send('Hello World!')
-    //   })
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+
+    // Load Express Middleware
+    const expressApp = await expressMiddleware(app);
+
+    // Load Passport Middleware
+    //const passport = await passportMiddleware(expessApp);
+
+    // Error Handler
+    app.use((error, request, response, next) => {
+      const { message, status } = error;
+      return response.status(status).send({ message });
+    });
     
+    // Start Server
     app.listen(PORT, () => {
         console.log(`Server listening on PORT ${PORT}`);
       })
