a5759dac0c965b106b1264781d33aa18b225f3f1
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2024-01-04 17:56:45 -0700

    Adding Account Page

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { useDispatch } from 'react-redux';
 import Header from './Header/Header';
 import Home from '../routes/Home/Home';
 import Register from '../routes/Register/Register';
 import Login from '../routes/Login/Login';
-//import PrivateRoute from './PrivateRoute/PrivateRoute';
 import Account from '../routes/Account/Account';
+import { checkLoginStatus } from '../store/auth/Auth.actions';
+import './App.css';
 
 function App() {
-  return (
-    <div style={{flex: 1}}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* Public Routes */}
-          <Route path='/' element={<Home/>} />
-          <Route path='register' element={<Register/>} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/account' element={<Account/>}/>
+    const dispatch = useDispatch();
 
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+    // Load user cart on entry to app
+    useEffect(() => {
+        async function isLoggedIn() {
+          await dispatch(checkLoginStatus());
+        }
+
+        isLoggedIn();
+    }, [dispatch])
+
+  return (
+      <div style={{flex: 1}}>
+          <BrowserRouter>
+          <Header />
+              <Routes>
+                  {/* Public Routes */}
+                  <Route path='/' element={<Home/>} />
+                  <Route path='register' element={<Register/>} />
+                  <Route path='/login' element={<Login/>}/>
+
+                  {/* Private Routes */}
+                  <Route exact path='/account' element={<Account/>} />
+
+
+              </Routes>
+          </BrowserRouter>
+      </div>
+    );
+};
 
 export default App;
